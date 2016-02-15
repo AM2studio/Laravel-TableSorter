d5a582d4e108b3971f5af2042ab4ab2622e002db
Filip Horvat <fico7489@gmail.com>
2016-02-15 14:43:32 +0100

    initial

---
+++ src/AM2Studio/Laravel/TableSorter/TableSorter.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace AM2Studio\Laravel\TableSorter;
+
+class TableSorter
+{
+	
+	public static function sort(array $headings, LengthAwarePaginator $paginator, array $config)
+    {
+        $sort_by   = $config['sort_by'];
+        $sort_type = $config['sort_type'];
+
+        $string = '';
+        foreach ($headings as $heading) {
+            $name  = $heading['name'];
+            $title = $heading['title'];
+
+            if ($sort_by != $name) {
+                $sort_type_this = 'ASC';
+            } else {
+                if ($sort_type == 'ASC') {
+                    $sort_type_this = 'DESC';
+                } else {
+                    $sort_type_this = 'ASC';
+                }
+            }
+
+            $paginator_tmp = clone $paginator;
+            $string .= sprintf($config['template'],
+                $paginator_tmp->appends(['sort_by' => $name, 'sort_type' => $sort_type_this])->url($paginator_tmp->currentPage()),
+                $title
+            );
+        }
+
+        echo $string;
+    }
+	
+}
