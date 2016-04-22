9c178b3152d3a055d613e2d35a1e19293dd84890
Filip Horvat <fico7489@gmail.com>
2016-04-22 15:43:05 +0200

    table sorter new version

--- src/AM2Studio/Laravel/TableSorter/TableSorter.php
+++ src/AM2Studio/Laravel/TableSorter/TableSorter.php
@@ -1,19 +1,60 @@
 <?php
 
-
 namespace AM2Studio\Laravel\TableSorter;
+
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Config;
 
 class TableSorter
 {
-    public static function sort(array $headings, \Illuminate\Pagination\LengthAwarePaginator $paginator, array $config)
+    //template for links
+    private $templateConfig;
+    //template for disabled links
+    private $templateDisabledConfig;
+
+    //default sort_by variable in url (sort_by)
+    private $sort_by_variable;
+    //default sort_type variable in url (sort_type)
+    private $sort_type_variable;
+
+    //classes which plugin uses for links
+    private $order_active_class;
+    private $order_asc_class;
+    private $order_desc_class;
+    private $order_next_asc_class;
+    private $order_next_desc_class;
+
+    public function __construct()
     {
-        $sort_by = $config['sort_by'];
-        $sort_type = $config['sort_type'];
+        //load from app config
+        $this->templateConfig = (Config::get('table-sorter.template')) ? Config::get('table-sorter.template') : '<th class="%s"><a href="%s"> %s </a></th>';
+        $this->templateDisabledConfig = (Config::get('table-sorter.templateDisabled')) ? Config::get('table-sorter.templateDisabled') : '<th class="%s"> %s </th>';
+        $this->sort_by_variable = (Config::get('table-sorter.sort_by_variable')) ? Config::get('table-sorter.sort_by_variable') : 'sort_by';
+        $this->sort_type_variable = (Config::get('table-sorter.sort_type_variable')) ? Config::get('table-sorter.sort_type_variable') : 'sort_type';
+        $this->order_active_class = (Config::get('table-sorter.order_active_class')) ? Config::get('table-sorter.order_active_class') : 'order-active';
+        $this->order_asc_class = (Config::get('table-sorter.order_asc_class')) ? Config::get('table-sorter.order_asc_class') : 'order-asc';
+        $this->order_desc_class = (Config::get('table-sorter.order_desc_class')) ? Config::get('table-sorter.order_desc_class') : 'order-desc';
+        $this->order_next_asc_class = (Config::get('table-sorter.order_next_asc_class')) ? Config::get('table-sorter.order_next_asc_class') : 'order-next-asc';
+        $this->order_next_desc_class = (Config::get('table-sorter.order_next_desc_class')) ? Config::get('table-sorter.order_next_desc_class') : 'order-next-desc';
+    }
+
+    public function sortTable($data)
+    {
+        $headings = $data['headings'];
+        $paginator = $data['paginator'];
+        $config = (isset($data['config'])) ? $data['config'] : [];
+
+        //load from headings
+        $sort_by = (isset($config['sort_by'])) ? $config['sort_by'] : Request::get($this->sort_by_variable);
+        $sort_type = (isset($config['sort_type'])) ? $config['sort_type'] : Request::get($this->sort_type_variable);
+        $template = (isset($config['template'])) ? $config['template'] : $this->templateConfig;
+        $templateDisabled = (isset($config['templateDisabled'])) ? $config['templateDisabled'] : $this->templateDisabledConfig;
 
         $string = '';
         foreach ($headings as $heading) {
-            $name = $heading['name'];
-            $title = $heading['title'];
+            $name = (isset($heading['name']))  ? $heading['name'] : '';
+            $title = (isset($heading['title'])) ? $heading['title'] : '';
+            $sort = (isset($heading['sort']))  ? $heading['sort'] : true;
 
             if ($sort_by != $name) {
                 $sort_type_this = 'ASC';
@@ -27,27 +68,71 @@ class TableSorter
 
             $class = '';
             if ($sort_by == $name) {
-				if ($sort_type_this == 'DESC') {
-					$class .= 'order-active order-asc ';
-				}else{
-					$class .= 'order-active order-desc ';
-				}
-            }
-			
-            if ($sort_type_this == 'DESC') {
-                $class .= 'order-next-desc';
-            } else {
-                $class .= 'order-next-asc';
+                if ($sort_type_this == 'ASC') {
+                    //$class .= 'order-active order-desc ';
+                    $class .= $this->order_active_class.' '.$this->order_desc_class.' ';
+                } else {
+                    //$class .= 'order-active order-asc ';
+                    $class .= $this->order_active_class.' '.$this->order_asc_class.' ';
+                }
             }
 
-            $paginator_tmp = clone $paginator;
-            $string .= sprintf($config['template'],
-                $class,
-                $paginator_tmp->appends(['sort_by' => $name, 'sort_type' => $sort_type_this])->url($paginator_tmp->currentPage()),
-                $title
-            );
+            if ($sort_type_this == 'ASC') {
+                //$class .= 'order-next-asc';
+                $class .= $this->order_next_asc_class;
+            } else {
+                //$class .= 'order-next-desc';
+                $class .= $this->order_next_desc_class;
+            }
+
+            if ($sort == true) {
+                $paginator_tmp = clone $paginator;
+                $string .= sprintf(
+                    $template,
+                    $class,
+                    $paginator_tmp->appends([$this->sort_by_variable => $name, $this->sort_type_variable => $sort_type_this])->url($paginator_tmp->currentPage()),
+                    $title
+                );
+            } else {
+                $string .= sprintf($templateDisabled, 'order-disabled', $title);
+            }
         }
 
         echo $string;
+    }
+
+    public function sortBySelect($data, $configForm)
+    {
+        $headings = $data['headings'];
+        $paginator = $data['paginator'];
+        $config = (isset($data['config'])) ? $data['config'] : [];
+
+        $sort_by = (isset($config['sort_by'])) ? $config['sort_by'] : Request::get($this->sort_by_variable);
+
+        $dataSelect = [];
+        foreach ($headings as $heading) {
+            $name = (isset($heading['name']))  ? $heading['name'] : '';
+            $title = (isset($heading['title'])) ? $heading['title'] : '';
+            $sort = (isset($heading['sort']))  ? $heading['sort'] : true;
+
+            if ($sort == true) {
+                $dataSelect[$name] = $title;
+            }
+        }
+
+        echo \Form::select($this->sort_by_variable, $dataSelect, $sort_by, $configForm);
+    }
+
+    public function sortTypeSelect($data, $configForm)
+    {
+        $headings = $data['headings'];
+        $paginator = $data['paginator'];
+        $config = (isset($data['config'])) ? $data['config'] : [];
+
+        $sort_type = (isset($config['sort_type'])) ? $config['sort_type'] : Request::get($this->sort_type_variable);
+
+        $dataSelect = ['ASC' => 'ASC', 'DESC' => 'DESC'];
+
+        echo \Form::select($this->sort_by_variable, $dataSelect, $sort_type, $configForm);
     }
 }
---
+++ src/AM2Studio/Laravel/TableSorter/config/table-sorter.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'template' => '<th class="%s"><a href="%s"> %s </a></th>',
+    'templateDisabled' => '<th class="%s"> %s </th>',
+    'sort_by_variable' => 'sort_by',
+    'sort_type_variable' => 'sort_type',
+    'order_active_class' => 'order-active',
+    'order_asc_class' => 'order-asc',
+    'order_desc_class' => 'order-desc',
+    'order_next_asc_class' => 'order-next-asc',
+    'order_next_desc_class' => 'order-next-desc',
+];
